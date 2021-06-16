2126352575d928fc85cf17843408ac3fdaebaef6
eficek <eficek@protonmail.com>
2021-06-16 04:42:30 -0500

    chore: set up heroku database url

--- server/db/database.js
+++ server/db/database.js
@@ -4,7 +4,7 @@ const pkg = require("../../package.json");
 const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}_test` : pkg.name;
 console.log(`Opening database connection to ${dbName}`);
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
 });
 
