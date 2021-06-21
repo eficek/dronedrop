9bbda4c1fab4f70d619f813d91d2d322a66f31b6
eficek <eficek@protonmail.com>
2021-06-21 05:24:00 -0500

    chore: heroku environment db + port

--- server/db/database.js
+++ server/db/database.js
@@ -5,7 +5,14 @@ const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}_test` : pkg.name;
 console.log(`Opening database connection to ${dbName}`);
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-  logging: false,
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = db;
